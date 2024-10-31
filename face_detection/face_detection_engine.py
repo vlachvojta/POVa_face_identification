c588e1a087a8590d41463ae055b6e504042faf26
Zuzana Hrklova <xhrklo00@stud.fit.vutbr.cz>
2024-10-31 22:35:16 +0100

    add basic face detection engine class

--- face_detection/face_detection_engine.py
+++ face_detection/face_detection_engine.py
@@ -1,10 +1,26 @@
+import cv2
+import torch
+import numpy as np
+from facenet_pytorch import MTCNN
+from PIL import Image
 
+class FaceDetectionEngine:
+	def __init__(self, device: str = 'cuda', keep_all=True):
+		self.device = device
+		self.model = MTCNN(keep_all=keep_all, device=self.device)
+  
+	def __call__ (self, image_path, output_path=None):
+		image = Image.open(image_path)
+		boxes, _ = self.model.detect(image)
+        
+		image_cv = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
 
-class FaceDetectionEngine():
-	...
-	# engine capable of detecting faces in an image (used by app or training)
-
-# TODO:
-# - find and test some face detection systems
-# - create engine, that can be used in an app or training.
-
+		if boxes is not None:
+			for box in boxes:
+				x1, y1, x2, y2 = map(int, box)
+				cv2.rectangle(image_cv, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        
+		if output_path:
+			cv2.imwrite(output_path, image_cv)
+        
+		return boxes if boxes is not None else []
