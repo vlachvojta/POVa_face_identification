a92fc53018a52062822ce5fef34da2bc16f1cc04
vlachvojta <vojtik.vlach@seznam.cz>
2024-12-19 12:16:45 +0100

    Add tinkering around with face_detection_engine + example outputs

--- face_detection/face_detection_engine.py
+++ face_detection/face_detection_engine.py
@@ -2,6 +2,7 @@ import numpy as np
 from facenet_pytorch import MTCNN
 from PIL import Image
 import matplotlib.pyplot as plt
+import cv2 as cv
 
 
 class FaceDetectionEngine:
@@ -26,6 +27,27 @@ class FaceDetectionEngine:
 				})
 
 		return results
+
+	def strict_detection(self, image, image_src: str = None):
+		# detect one face
+		face = self.model(image)
+
+		# check if face was detected
+		if face is None:
+			print(f"Skipping squarify around face on image {image_src} as no face was detected.")
+			face = cv.resize(image, (160, 160))
+
+		# check face has shape [C, H, W]
+		if not face.ndim == 3:
+			raise ValueError(f"Face should have 3 dimensions (C, H, W) or (H, W, C), got {face.ndim} dimensions with shape {face.shape}")
+
+		if face.shape[0] != 3 and face.shape[2] == 3:
+			face = face.transpose(2, 0, 1)
+
+		if face.shape[1] != 160 or face.shape[2] != 160:
+			raise ValueError(f"Face should have shape (3, 160, 160), got {face.shape}")
+
+		return face
 
 	def crop_faces(self, image, threshold=0.8) -> tuple[list[np.ndarray], list[float]]:
 		results = self(image)
@@ -70,6 +92,7 @@ def show_image_with_boxes(image, result):
 			dot = Circle((point[0], point[1]), radius=2, color='red')
 			ax.add_patch(dot)
 
+	plt.savefig('image_with_boxes.png')
 	plt.show()
 
 
@@ -88,6 +111,27 @@ def example_usage():
 		plt.imshow(face)
 		plt.show()
 
+def testing_detection_with_MTCNN():
+	# mtcnn = MTCNN(keep_all=True)
+	mtcnn = MTCNN()
+
+	image = Image.open('example.webp')
+	face = mtcnn(image)
+	print(f'Faces ({face.shape}): {face}')
+
+	# face is in float [-1, 1] range, convert to uint8 [0, 255]
+	face = (face + 1) * 127.5
+	print(f'\tface ({face.shape}): {face}')
+	face = face.numpy().astype(np.uint8)
+
+	# face has shape [C, H, W], convert to [H, W, C]
+	face = np.transpose(face, (1, 2, 0))
+	print(f'\tface ({face.shape}): {face}')
+
+	# save face to file
+	Image.fromarray(face).save('face.png')
 
 if __name__ == "__main__":
 	example_usage()
+	# testing_detection_with_MTCNN()
+	# show_image_with_boxes()
